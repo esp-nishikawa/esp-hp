22309d78cf5a01a7ab64129df698c5e8e09b2460
esp-nishikawa <t.nishikawa@esoftpowers.com>
2020-07-29 20:51:03 +0900

    tableにtransitionを追加

--- src/components/ThePerformance.vue
+++ src/components/ThePerformance.vue
@@ -67,6 +67,8 @@ export default {
       ],
       page: 1,
       itemsPerPage: 5,
+      sortAsc: false,
+      transitionName: 'up',
     };
   },
   computed: {
@@ -74,7 +76,33 @@ export default {
       return Math.ceil(this.items.length / this.itemsPerPage);
     },
     activeItems() {
-      return this.items.slice(this.itemsPerPage * (this.page - 1), this.itemsPerPage * this.page);
+      return this.items.slice()
+        .sort((a, b) => {
+          const comparison = a.year.match(/\d+/) - b.year.match(/\d+/);
+          return this.sortAsc ? comparison : -comparison;
+        })
+        .slice(this.itemsPerPage * (this.page - 1), this.itemsPerPage * this.page);
+    },
+  },
+  watch: {
+    page(newVal, oldVal) {
+      this.transitionName = newVal < oldVal ? 'right' : 'left';
+    },
+  },
+  methods: {
+    toggleSort() {
+      this.sortAsc = !this.sortAsc;
+      this.transitionName = this.sortAsc ? 'up' : 'down';
+    },
+    prevPage() {
+      if (this.page > 1) {
+        this.page--;
+      }
+    },
+    nextPage() {
+      if (this.page < this.pageCount) {
+        this.page++;
+      }
     },
   },
 };
@@ -82,22 +110,40 @@ export default {
 
 <template>
   <v-sheet>
-    <v-simple-table height="300">
+    <v-simple-table
+      height="300"
+      fixed-header
+      v-touch="{
+        left: nextPage,
+        right: prevPage,
+      }"
+    >
       <template>
         <thead>
           <tr>
-            <th class="table-th column-year">年度</th>
+            <th
+              class="table-th column-year"
+              :class="{'asc': sortAsc}"
+              @click="toggleSort"
+            >
+              <v-icon small color="white" class="mr-1">arrow_downward</v-icon>年度
+            </th>
             <th class="table-th column-sales">売上高</th>
             <th class="table-th column-employees">社員数</th>
             <th class="table-th column-engineers">内技術者数</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody
+          is="transition-group"
+          :name="transitionName"
+          leave-active-class="v-leave-active"
+          enter-active-class="v-enter-active"
+        >
           <tr v-for="item in activeItems" :key="item.year">
-            <td class="column-year">{{ item.year }}</td>
-            <td class="column-sales">{{ item.sales }}</td>
-            <td class="column-employees">{{ item.employees }}</td>
-            <td class="column-engineers">{{ item.engineers }}</td>
+            <td class="table-td column-year">{{ item.year }}</td>
+            <td class="table-td column-sales">{{ item.sales }}</td>
+            <td class="table-td column-employees">{{ item.employees }}</td>
+            <td class="table-td column-engineers">{{ item.engineers }}</td>
           </tr>
         </tbody>
       </template>
@@ -116,14 +162,25 @@ export default {
   color: #fff!important;
   font-size: 15px!important;
   font-weight: 500!important;
-  border: 1px solid rgba(255,255,255,.5);
-  background: linear-gradient(to bottom, rgba(30,136,229,.7), rgba(30,136,229,.9));
-  text-shadow: 0 -1px 0 rgba(30,136,229,.9);
+  border: 1px solid rgba(255,255,255,.5)!important;
+  background: linear-gradient(to bottom, rgba(30,136,229,.7), rgba(30,136,229,.9))!important;
+  text-shadow: 0 -1px 0 rgba(30,136,229,.9)!important;
+}
+
+.table-td {
+  color: rgba(0,0,0,.87)!important;
+  font-size: 14px!important;
+  font-weight: 400!important;
 }
 
 .column-year {
   width: 70px;
   text-align: right!important;
+  cursor: pointer;
+}
+
+.column-year.asc .v-icon {
+  transform: rotate(180deg);
 }
 
 .column-sales {
@@ -141,7 +198,38 @@ export default {
   text-align: right!important;
 }
 
+.v-leave-active {
+  position: absolute;
+  transition: none;
+  opacity: 0;
+}
+
+.v-enter-active {
+  transition: opacity .3s, transform .6s;
+}
+
+.up-enter {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.down-enter {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.left-enter {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.right-enter {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
 >>> table {
+  overflow: hidden;
   border-bottom: thin solid rgba(0,0,0,.12);
 }
 </style>
